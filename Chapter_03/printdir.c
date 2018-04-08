0a3272d66b27a89719054d1e07dc713e788978f5
Humphrey.Zhu <zhufang1987@yeah.net>
2018-04-08 20:01:03 +0800

    Improve printdir.c.

--- Chapter_03/printdir.c
+++ Chapter_03/printdir.c
@@ -35,10 +35,14 @@ void printdir(char *dir, int depth)
 	closedir(dp);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	printf("Directory scan of /home:\n");
-	printdir("/home", 0);
+	char *topdir = ".";
+	if (argc >= 2)
+		topdir = argv[1];
+
+	printf("Directory scan of %s\n", topdir);
+	printdir(topdir, 0);
 	printf("done.\n");
 
 	return 0;
