ab36b64316ec94e6f4bdcbc213ccce8f43783104
Humphrey.Zhu <zhufang1987@yeah.net>
2018-08-28 21:43:30 +0800

    Add cinfo.c.

---
+++ Chapter_10/cinfo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+#ifdef DEBUG
+	printf("Compiled: " __DATE__ " at " __TIME__ "\n");
+	printf("This is line %d of file %s\n", __LINE__, __FILE__);
+#endif
+	printf("hello world\n");
+
+	return 0;
+}
