332e425149cfe062ab995171371795311a6a9766
Humphrey.Zhu <zhufang1987@yeah.net>
2018-09-14 21:46:12 +0800

    Add assert.c and debug4.c.There're some wierd things about Chapter_07 and Chapter_08.

---
+++ Chapter_10/assert.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+
+double my_sqrt(double x)
+{
+	assert(x >= 0.0);
+	return sqrt(x);
+}
+
+int main()
+{
+	printf("sqrt +2 = %g\n", my_sqrt(2.0));
+	printf("sqrt -2 = %g\n", my_sqrt(-2.0));
+
+	return 0;
+}
