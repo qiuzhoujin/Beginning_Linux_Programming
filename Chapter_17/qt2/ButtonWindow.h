c160db0cdafda36c9ba62e0804928fa7292e2062
Humphrey.Zhu <zhufang1987@yeah.net>
2019-08-30 21:43:33 +0800

    Add ButtonWindow.

---
+++ Chapter_17/qt2/ButtonWindow.h
@@ -0,0 +1,14 @@
+//#include <qmainwindow.h>
+#include <QMainWindow>
+
+class ButtonWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	ButtonWindow(QWidget *parent = 0, const char *name = 0);
+	virtual ~ButtonWindow();
+
+private slots:
+	void Clicked();
+};
